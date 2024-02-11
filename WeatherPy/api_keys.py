37615d3a75a4850fa9c3728b7bf0b51ba1364a90
mmacinnis <153472336+mmacinnis@users.noreply.github.com>
2024-02-11 17:56:02 -0500

    Add files via upload

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "1ff4b8f07fa9c9f7273388c4dc0ba1f4"
+# example link: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1ff4b8f07fa9c9f7273388c4dc0ba1f4
+
+# Geoapify API Key
+geoapify_key = "8f2321edcfa04f509d1712b16e446460"
